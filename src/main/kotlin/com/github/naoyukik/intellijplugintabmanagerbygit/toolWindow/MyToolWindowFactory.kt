4b32624bad9502e6c660d654663942c481338e6b
GitHub Action <action@github.com>
2024-11-10 11:16:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/naoyukik/intellijplugintabmanagerbygit/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugintabmanagerbygit/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.naoyukik.intellijplugintabmanagerbygit.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.naoyukik.intellijplugintabmanagerbygit.MyBundle
+import com.github.naoyukik.intellijplugintabmanagerbygit.services.MyProjectService
 import javax.swing.JButton
 
 
