b64587bcab9ddbf872049677518f3ef7f5c64819
Kodama Naoyuki <mmaga@dstribe.net>
2025-12-17 21:06:23 +0900

    feat: add action to pin all currently open files

---
+++ src/main/kotlin/com/github/naoyukik/ijtabmanagerex/application/TabPinningService.kt
@@ -0,0 +1,19 @@
+package com.github.naoyukik.ijtabmanagerex.application
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.project.Project
+import com.intellij.util.concurrency.annotations.RequiresEdt
+
+@Service(Service.Level.PROJECT)
+class TabPinningService(val project: Project) {
+    @RequiresEdt
+    fun pinAllOpenFiles() {
+        val editorManager = FileEditorManagerEx.getInstanceEx(project)
+        editorManager.windows.forEach { window ->
+            window.fileList.forEach { file ->
+                window.setFilePinned(file, true)
+            }
+        }
+    }
+}
